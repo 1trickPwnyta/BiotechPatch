30da8f1b823743cb4009e97583162b9bd0ccdb38
1trickPwnyta <sarahpace37@gmail.com>
2025-05-12 23:02:02 -0700

    Purge pawn list to avoid errors on load

--- 1.5/Source/ChildrenGoByFirstName/GameComponent_ChildrenGoByFirstName.cs
+++ 1.5/Source/ChildrenGoByFirstName/GameComponent_ChildrenGoByFirstName.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using RimWorld;
+using System.Collections.Generic;
 using Verse;
 
 namespace BiotechPatch.ChildrenGoByFirstName
@@ -13,8 +14,28 @@ namespace BiotechPatch.ChildrenGoByFirstName
 
         public GameComponent_ChildrenGoByFirstName(Game _) { }
 
+        private void Purge()
+        {
+            List<NameTriple> keysToRemove = new List<NameTriple>();
+            foreach (KeyValuePair<NameTriple, Pawn> pair in pawns)
+            {
+                if (pair.Value == null || !PawnsFinder.All_AliveOrDead.Contains(pair.Value))
+                {
+                    keysToRemove.Add(pair.Key);
+                }
+            }
+            foreach (NameTriple name in keysToRemove)
+            {
+                pawns.Remove(name);
+            }
+        }
+
         public override void ExposeData()
         {
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                Purge();
+            }
             Scribe_Collections.Look(ref pawns, "pawns", LookMode.Deep, LookMode.Reference, ref keysWorkingList, ref valuesWorkingList);
         }
     }
