c7be3303cc28328777eb210013ee6831c2a9d2b0
1trickPwnyta <sarahpace37@gmail.com>
2025-11-27 18:16:53 -0800

    Put mech energy level in colonist bar instead of mood

---
+++ 1.6/Source/MechsInColonistBar/CompatibilityPatch_ColoredMoodBar.cs
@@ -0,0 +1,100 @@
+﻿using ColoredMoodBar13;
+using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Verse;
+
+namespace BiotechPatch.MechsInColonistBar
+{
+    [StaticConstructorOnStartup]
+    public static class CompatibilityPatch_ColoredMoodBar
+    {
+        static CompatibilityPatch_ColoredMoodBar()
+        {
+            Type type = AccessTools.TypeByName("ColoredMoodBar13.MoodCache");
+            if (type != null)
+            {
+                var harmony = new Harmony(BiotechPatchMod.PACKAGE_ID);
+                harmony.Patch(type.Method("DoMood"), transpiler: typeof(CompatibilityPatch_ColoredMoodBar_DoMood).Method(nameof(CompatibilityPatch_ColoredMoodBar_DoMood.Transpiler)));
+            }
+        }
+    }
+
+    public static class CompatibilityPatch_ColoredMoodBar_DoMood
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            List<CodeInstruction> instructionsList = instructions.ToList();
+
+            Label trueLabel = il.DefineLabel();
+            instructionsList.InsertRange(3, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Call, typeof(Pawn).PropertyGetter(nameof(Pawn.IsColonyMech))),
+                new CodeInstruction(OpCodes.Brtrue_S, trueLabel)
+            });
+
+            int index = instructionsList.FindIndex(i => i.Calls(typeof(Need).PropertyGetter(nameof(Need.CurLevel))));
+            instructionsList[index - 6].labels.Add(trueLabel);
+            instructionsList[index - 1] = new CodeInstruction(OpCodes.Call, typeof(CompatibilityPatch_ColoredMoodBar_DoMood).Method(nameof(GetMoodNeed)));
+
+            index = instructionsList.FindIndex(i => i.Calls(typeof(Need).PropertyGetter(nameof(Need.CurLevelPercentage))));
+            instructionsList[index - 1] = new CodeInstruction(OpCodes.Call, typeof(CompatibilityPatch_ColoredMoodBar_DoMood).Method(nameof(GetMoodNeed)));
+
+            Label nonMechLabel = il.DefineLabel();
+            index = instructionsList.FindLastIndex(i => i.LoadsField(typeof(MoodCache).Field("BreakThresholdExtreme")));
+            instructionsList[index].labels.Add(nonMechLabel);
+            instructionsList.InsertRange(index, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_1),
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, typeof(CompatibilityPatch_ColoredMoodBar_DoMood).Method(nameof(SetParametersForEnergy))),
+                new CodeInstruction(OpCodes.Brfalse_S, nonMechLabel),
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Ret)
+            });
+
+            return instructionsList;
+        }
+
+        private static Need GetMoodNeed(Pawn_NeedsTracker needs) => BiotechPatchSettings.MechsInColonistBar ? (needs.mood as Need ?? needs.energy) : needs.mood;
+
+        private static bool SetParametersForEnergy(Pawn pawn, MoodCache cache)
+        {
+            if (BiotechPatchSettings.MechsInColonistBar && pawn.IsColonyMech)
+            {
+                MechanitorControlGroup group = pawn.GetMechControlGroup();
+                if (group == null || pawn.needs.energy.CurLevelPercentage > group.mechRechargeThresholds.min)
+                {
+                    cache.MoodLevel = MoodLevel.Neutral;
+                    cache.MoodTexture = Main.neutralTex;
+                    cache.MoodColor = Main.Settings.Neutral;
+                }
+                else if (pawn.needs.energy.CurLevelPercentage > group.mechRechargeThresholds.min / 2)
+                {
+                    cache.MoodLevel = MoodLevel.Minor;
+                    cache.MoodTexture = Main.minorBreakTex;
+                    cache.MoodColor = Main.Settings.Minor;
+                }
+                else if (pawn.needs.energy.CurLevelPercentage > 0f)
+                {
+                    cache.MoodLevel = MoodLevel.Major;
+                    cache.MoodTexture = Main.majorBreakTex;
+                    cache.MoodColor = Main.Settings.Major;
+                }
+                else
+                {
+                    cache.MoodLevel = MoodLevel.Extreme;
+                    cache.MoodTexture = Main.extremeBreakTex;
+                    cache.MoodColor = Main.Settings.Extreme;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 1.6/Source/MechsInColonistBar/Patch_ColonistBarColonistDrawer.cs
@@ -0,0 +1,79 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+
+namespace BiotechPatch.MechsInColonistBar
+{
+    [HarmonyPatch(typeof(ColonistBarColonistDrawer))]
+    [HarmonyPatch(nameof(ColonistBarColonistDrawer.DrawColonist))]
+    public static class Patch_ColonistBarColonistDrawer
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+        {
+            List<CodeInstruction> instructionsList = instructions.ToList();
+
+            Label label = il.DefineLabel();
+            int index = instructionsList.FindIndex(i => i.Calls(typeof(GUI).Method(nameof(GUI.DrawTexture), new[] { typeof(Rect), typeof(Texture) })));
+            instructionsList.InsertRange(index + 4, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Call, typeof(Pawn).PropertyGetter(nameof(Pawn.IsColonyMech))),
+                new CodeInstruction(OpCodes.Brtrue_S, label)
+            });
+
+            index = instructionsList.FindIndex(i => i.Calls(typeof(Need).PropertyGetter(nameof(Need.CurLevelPercentage))));
+            instructionsList[index - 9].labels.Add(label);
+            instructionsList[index - 1] = new CodeInstruction(OpCodes.Call, typeof(Patch_ColonistBarColonistDrawer).Method(nameof(GetMoodNeed)));
+
+            index = instructionsList.FindIndex(i => i.Calls(typeof(MoodThresholdExtensions).Method(nameof(MoodThresholdExtensions.CurrentMoodThresholdFor))));
+            instructionsList.InsertRange(index - 1, new[]
+            {
+                new CodeInstruction(OpCodes.Pop),
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Call, typeof(Patch_ColonistBarColonistDrawer).Method(nameof(ShouldShowMechEnergy)))
+            });
+
+            index = instructionsList.FindIndex(i => i.Calls(typeof(MoodThresholdExtensions).Method(nameof(MoodThresholdExtensions.CurrentMoodThresholdFor))));
+            instructionsList.InsertRange(index + 1, new[]
+            {
+                new CodeInstruction(OpCodes.Ldarg_2),
+                new CodeInstruction(OpCodes.Call, typeof(Patch_ColonistBarColonistDrawer).Method(nameof(GetMoodThreshold)))
+            });
+
+            return instructionsList;
+        }
+
+        private static Need GetMoodNeed(Pawn_NeedsTracker needs) => BiotechPatchSettings.MechsInColonistBar ? (needs.mood as Need ?? needs.energy) : needs.mood;
+
+        private static bool ShouldShowMechEnergy(Pawn pawn) => Prefs.VisibleMood && BiotechPatchSettings.MechsInColonistBar && pawn.IsColonyMech && !pawn.Dead;
+
+        private static MoodThreshold GetMoodThreshold(MoodThreshold original, Pawn pawn)
+        {
+            if (BiotechPatchSettings.MechsInColonistBar && pawn.IsColonyMech)
+            {
+                MechanitorControlGroup group = pawn.GetMechControlGroup();
+                if (group == null || pawn.needs.energy.CurLevelPercentage > group.mechRechargeThresholds.min)
+                {
+                    return MoodThreshold.None;
+                }
+                if (pawn.needs.energy.CurLevelPercentage > group.mechRechargeThresholds.min / 2)
+                {
+                    return MoodThreshold.Minor;
+                }
+                if (pawn.needs.energy.CurLevelPercentage > 0f)
+                {
+                    return MoodThreshold.Major;
+                }
+                return MoodThreshold.Extreme;
+            }
+            else
+            {
+                return original;
+            }
+        }
+    }
+}
